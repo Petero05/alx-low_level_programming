df7a8020e2316ba99ba6f83b0172e228fc799bbf
Jean Pierre <tuyapeter@gmail.com>
2023-03-16 13:54:28 -0700

    7th Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,11 @@
 /*
  * main - Entry point
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
+//Display of size of various types
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
