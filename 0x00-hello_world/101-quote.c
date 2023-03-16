7cdba40df0c04a58857a42ec377a439918fec16d
Jean Pierre <tuyapeter@gmail.com>
2023-03-16 14:35:30 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unstd.h>
 
 /**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *followed by a newline, to standard error.
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful"  - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
