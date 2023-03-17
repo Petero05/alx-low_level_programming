cc64c584eadb3225c1a17ed7dcf76bce2817950b
Jean Pierre <tuyapeter@gmail.com>
2023-03-17 15:50:58 -0700

    all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,18 +7,19 @@
  */
 int main(void)
 {
-
-	int n;
+        int n;
         int m;
 
-	for (n = 48 ; n <= 57; n++)
-	{
-		putchar(n);
-	}
-	for (m = 97 ; m <= 102; m++)
+for (n = 48 ; n <= 57; n++)
+        {
+                putchar(n);
+        }
+        
+for (m = 97 ; m <= 102; m++)
         {
                 putchar(m);
         }
-	putchar('\n');
-	return (0);
+
+        putchar('\n');
+return (0);
 }
