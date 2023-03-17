979e33c63ac962b47ba4ee8c30e87c4ee68afd1b
Jean Pierre <tuyapeter@gmail.com>
2023-03-17 16:06:22 -0700

    Check betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,14 +12,13 @@ int main(void)
 
 for (n = 48 ; n <= 57; n++)
         {
-                putchar(n);
-        }
-        
-for (m = 97 ; m <= 102; m++)
-        {
-                putchar(m);
-        }
+putchar(n);
+}
 
-        putchar('\n');
+for (m = 97 ; m <= 102; m++)
+{
+	putchar(m);
+}
+putchar('\n');
 return (0);
 }
