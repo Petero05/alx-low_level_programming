0101a8241d3cacfad4b7f22beeb64c0b90888d28
Jean Pierre <tuyapeter@gmail.com>
2023-03-17 16:01:17 -0700

    all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,24 +2,24 @@
 
 /**
  * main -Entry point
- *
- * Returne - Always 0 (Success)
+ * Description: 'combinations of single-digit numbers.'
+ * Return:   Always 0 (Success)
  *
  */
 int main(void)
 {
-int n;
+	int n;
 
-for (n = 48; n <= 57; n++)
+	for (n = 48; n <= 57; n++)
+	{
+	putchar(n);
+	if (n == 57)
 {
-putchar(n);
-if (n == 57)
-{
-continue;
+	continue;
 }
-putchar(',');
-putchar(' ');
-}
-putchar('\n');
-return (0);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
