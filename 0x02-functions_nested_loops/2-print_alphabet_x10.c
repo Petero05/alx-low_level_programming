95791f107263eec60980ecf1cf57d578d1625ac8
Jean Pierre <tuyapeter@gmail.com>
2023-03-21 12:59:07 -0700

    2-print_alphabet 10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function  that prints 10X alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+        int letter;
+        char j;
+
+        for (letter = 1; letter <= 10; letter++)
+	{
+	 for (j = 'a'; j <= 'z'; j++)
+	  _putchar(j); 
+       	_putchar('\n');
+	}
+}
