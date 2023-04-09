b0141565f851a81ad97b81ed7c048c3c0a27718e
Jean Pierre <tuyapeter@gmail.com>
2023-04-09 06:48:40 -0700

    Static Libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - prints 1 or 0 depending on input
+ * @c: input type variable
+ *
+ * Description: checks for uppercase letters
+ * Return: 1 for upper letter or 0 for any else.
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
