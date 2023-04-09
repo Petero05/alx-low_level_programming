99feeac542d046e754e45466f602d49b8cf986ee
Jean Pierre <tuyapeter@gmail.com>
2023-04-09 08:41:50 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,23 +1,32 @@
 #include "main.h"
 /**
- * _strpbrk - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
  */
+
 char *_strpbrk(char *s, char *accept)
 {
-        int k;
+	int i, j;
+	char *p;
 
-        while (*s)
-        {
-                for (k = 0; accept[k]; k++)
-                {
-                if (*s == accept[k])
-                return (s);
-                }
-        s++;
-        }
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
 
-return ('\0');
+	return (0);
 }
